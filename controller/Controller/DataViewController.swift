cc2dad63bc49bd9b64f07c7bd773d2b798839dee
Joël Gähwiler <joel.gaehwiler@gmail.com>
2017-10-07 23:55:45 +0200

    boostrapped the controller ap

---
+++ controller/Controller/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  Controller
+//
+//  Created by Joël Gähwiler on 07.10.17.
+//  Copyright © 2017 Through Momentum. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
