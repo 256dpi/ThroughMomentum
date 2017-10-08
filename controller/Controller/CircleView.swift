8636f7ec8b322e9e3037e9f9878b23e520df7b3b
Joël Gähwiler <joel.gaehwiler@gmail.com>
2017-10-08 11:09:52 +0200

    added basic circleview

--- controller/Controller/CircleView.swift
+++ controller/Controller/CircleView.swift
@@ -9,35 +9,67 @@
 import UIKit
 
 class CircleView: UIView {
-    var arc: UIView?
-    var text: UILabel?
+    var circleView: UIView?
+    var idLabel: UILabel?
+    var positionLabel: UILabel?
+    var distanceLabel: UILabel?
+    
+    var position: Double {
+        get { return Double(positionLabel?.text ?? "0") ?? 0 }
+        set { positionLabel?.text = String(newValue) }
+    }
+    
+    var distance: Double {
+        get { return Double(distanceLabel?.text ?? "0") ?? 0 }
+        set { distanceLabel?.text = String(newValue) }
+    }
+    
+    var motion: Bool {
+        get { return circleView?.layer.borderWidth == 3 }
+        set { circleView!.layer.borderWidth = newValue ? 3 : 1 }
+    }
     
     required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        prepare()
+        fatalError("This class does not support NSCoding")
     }
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        prepare()
     }
     
-    func prepare() {
+    func prepare(id: Int) {
         // set background color
         backgroundColor = UIColor.clear
     
         // add arc view
-        arc = UIView(frame: CGRect(origin: CGPoint.zero, size: frame.size))
-        arc!.layer.cornerRadius = frame.width/2
-        arc!.layer.borderWidth = 1.0
-        arc!.layer.borderColor = UIColor.white.cgColor
-        addSubview(arc!)
+        circleView = UIView(frame: CGRect(x: 0, y: 0, width: frame.height, height: frame.height))
+        circleView!.layer.cornerRadius = frame.height/2
+        circleView!.layer.borderWidth = 1.0
+        circleView!.layer.borderColor = UIColor.white.cgColor
+        addSubview(circleView!)
         
         // add text view
-        text = UILabel(frame: CGRect(origin: CGPoint.zero, size: frame.size))
-        text!.textAlignment = .center
-        text!.textColor = .white
-        text!.text = "0.0"
-        addSubview(text!)
+        idLabel = UILabel(frame: CGRect(x: 0, y: 0, width: frame.height, height: frame.height))
+        idLabel!.textAlignment = .center
+        idLabel!.font = idLabel!.font.withSize(14)
+        idLabel!.textColor = .white
+        idLabel!.text = String(format: "%02d", id)
+        addSubview(idLabel!)
+        
+        // add position label
+        positionLabel = UILabel(frame: CGRect(x: frame.width / 2 + 5, y: frame.height / 5, width: frame.width / 2 - 5, height: frame.height / 5))
+        positionLabel!.textAlignment = .left
+        positionLabel!.font = positionLabel!.font.withSize(10)
+        positionLabel!.textColor = .white
+        positionLabel!.text = "120"
+        addSubview(positionLabel!)
+        
+        // add distance label
+        distanceLabel = UILabel(frame: CGRect(x: frame.width / 2 + 5, y: frame.height / 5 * 3, width: frame.width / 2 - 5, height: frame.height / 5))
+        distanceLabel!.textAlignment = .left
+        distanceLabel!.font = distanceLabel!.font.withSize(10)
+        distanceLabel!.textColor = .white
+        distanceLabel!.text = "70"
+        addSubview(distanceLabel!)
     }
 }
--- controller/Controller/MainViewController.swift
+++ controller/Controller/MainViewController.swift
@@ -8,9 +8,61 @@
 
 import UIKit
 
+let lightsPerRow = 8
+let lightsPerColumn = 6
+let padding: Double = 150
+
+let offColor = UIColor(white: 0.1, alpha: 1)
+let onColor = UIColor(white: 1, alpha: 1)
+
 class MainViewController: UIViewController {
+    var container: UIView?
+    var circleViews: [[CircleView]]?
+    
+    var fw: Double = 0
+    var fh: Double = 0
+    var gx: Double = 0
+    var gy: Double = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // create container
+        container = UIView(frame: view.frame)
+        view.addSubview(container!)
+        
+        // allocate array
+        circleViews = [[CircleView]]()
+        
+        // get frame size
+        fw = Double(container!.frame.width)
+        fh = Double(container!.frame.height)
+        
+        // calcuate gaps
+        gx = (fw-(2.0*padding))/Double(lightsPerRow-1)
+        gy = (fh-(2.0*padding))/Double(lightsPerColumn-1)
+        
+        // create all circles
+        for y in 0..<lightsPerColumn {
+            // add rows
+            circleViews!.insert([CircleView](), at: y)
+            
+            for x in 0..<lightsPerRow {
+                // calculate position
+                let xx = padding+Double(x)*gx
+                let yy = padding+Double(y)*gy
+                
+                // create view
+                let cv = CircleView(frame: CGRect(x: xx-50, y: yy-20, width: 100, height: 40))
+                cv.prepare(id: y * lightsPerRow + x + 1)
+                
+                // add to view
+                container!.addSubview(cv)
+                
+                // add to arrays
+                circleViews![y].insert(cv, at: x)
+            }
+        }
     }
     
     override var prefersStatusBarHidden: Bool {
