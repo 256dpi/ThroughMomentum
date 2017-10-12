b98c3ecbaf2e0724054cdce3a4e622e823575c5e
Joël Gähwiler <joel.gaehwiler@gmail.com>
2017-10-12 10:54:40 +0200

    bumped version

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -363,7 +363,7 @@ static void loop() {
 }
 
 static naos_config_t config = {.device_type = "vas17",
-                               .firmware_version = "0.3.0",
+                               .firmware_version = "0.4.0",
                                .ping_callback = ping,
                                .loop_callback = loop,
                                .loop_interval = 0,
