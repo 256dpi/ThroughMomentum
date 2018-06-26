4b2ea57a36c81de79221b42e127832b98c5643c7
Joël Gähwiler <joel.gaehwiler@gmail.com>
2018-06-26 11:37:08 +0200

    cleanup

--- firmware/src/led.h
+++ firmware/src/led.h
@@ -9,11 +9,11 @@ void led_init();
 /**
  * Set LED brightness.
  *
- * @param r - Value from 0 to 1023.
- * @param g - Value from 0 to 1023.
- * @param b - Value from 0 to 1023.
- * @param w - Value from 0 to 1023.
- * @param t - The time for the fade.
+ * @param r Value from 0 to 1023.
+ * @param g Value from 0 to 1023.
+ * @param b Value from 0 to 1023.
+ * @param w Value from 0 to 1023.
+ * @param t The time for the fade.
  */
 void led_set(int r, int g, int b, int w, int t);
 
--- firmware/src/mot.h
+++ firmware/src/mot.h
@@ -9,7 +9,7 @@ void mot_init();
 /**
  * Set motor state.
  *
- * @param speed - Number from -1024 to 1024.
+ * @param speed Number from -1024 to 1024.
  */
 void mot_set(int speed);
 
