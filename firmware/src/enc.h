f58cb577cd44252a41bbb6131e4504305af74393
Joël Gähwiler <joel.gaehwiler@gmail.com>
2018-06-26 12:46:22 +0200

    added docs

--- firmware/src/enc.h
+++ firmware/src/enc.h
@@ -3,8 +3,16 @@
 
 #include <stdbool.h>
 
+/**
+ * Callback is execute with rotational change up to a frequency of 1ms.
+ *
+ * @param rot Rotations.
+ */
 typedef void (*enc_callback_t)(double rot);
 
+/**
+ * Initialize the encoder sub system.
+ */
 void enc_init(enc_callback_t);
 
 #endif  // ENC_H
