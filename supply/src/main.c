470f4dbea860a0cb370ae7a94a3b1c746c8c4a90
Joël Gähwiler <joel.gaehwiler@gmail.com>
2018-07-26 13:14:48 +0200

    added ping callback and set initial relay state

--- supply/src/main.c
+++ supply/src/main.c
@@ -8,11 +8,16 @@ static bool r1 = false;
 static bool r2 = false;
 static bool r3 = false;
 
+static naos_status_t st;
+
 static void update(const char *param, const char *value) {
   rls_set(r1, r2, r3);
 }
 
 static void status(naos_status_t status) {
+  // save status
+  st = status;
+
   switch (status) {
     case NAOS_DISCONNECTED:
       led_set(false, false);
@@ -26,16 +31,23 @@ static void status(naos_status_t status) {
   }
 }
 
+static void ping() {
+  led_set(true, true);
+  naos_delay(300);
+  status(st);
+}
+
 static naos_param_t params[3] = {
     {.name = "relay-1", .type = NAOS_BOOL, .default_b = false, .sync_b = &r1},
     {.name = "relay-2", .type = NAOS_BOOL, .default_b = false, .sync_b = &r2},
     {.name = "relay-3", .type = NAOS_BOOL, .default_b = false, .sync_b = &r3},
 };
 
-static naos_config_t config = {.device_type = "supply",
-                               .firmware_version = "0.1.0",
+static naos_config_t config = {.device_type = "tm-ps",
+                               .firmware_version = "0.2.0",
                                .parameters = params,
                                .num_parameters = 3,
+                               .ping_callback = ping,
                                .update_callback = update,
                                .status_callback = status};
 
@@ -48,4 +60,7 @@ void app_main() {
 
   // initialize naos
   naos_init(&config);
+
+  // set relays
+  rls_set(r1, r2, r3);
 }
